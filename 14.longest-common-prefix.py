bdd1bf14bb476f4ed7d9fa69f528329fd2243336
YutaNaito <md17066@shibaura-it.ac.jp>
2022-08-25 01:09:56 +0900

    add: solution at 14.longest-common-prefix

---
+++ 14.longest-common-prefix.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=14 lang=python3
+#
+# [14] Longest Common Prefix
+#
+
+# @lc code=start
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs[0]:
+            return ""
+
+        result = ""
+        strs_min_len = min([len(_str) for _str in strs])
+        for idx in range(strs_min_len):
+
+            common_prefix = ""
+            for _str in strs:
+                if common_prefix == "":
+                    common_prefix = _str[: idx + 1]
+                    continue
+                if common_prefix != _str[: idx + 1]:
+                    return result
+            result = common_prefix
+
+        return result
+
+
+# @lc code=end
